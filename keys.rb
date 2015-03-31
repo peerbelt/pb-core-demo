a108bfb3ae5d3c6758f37b2ec4e55683c5f92c19
DD <dpd_damyanov@abv.bg>
2015-03-31 16:27:39 +0300

    test

--- keys.rb
+++ keys.rb
@@ -3,14 +3,13 @@
 require rubygems;
 require fog;
 
-docker_id_rsa=i"/home/ubuntu/.docker/machines/cd-eu-peerbelt-#{ENV['CIRCLE_BUILD_NUM]}/id_rsa"
+docker_id_rsa="/home/ubuntu/.docker/machines/cd-eu-peerbelt-#{ENV['CIRCLE_BUILD_NUM]}/id_rsa"
 
-service = Fog::Storage.new({
-    :provider            => 'Rackspace',         # Rackspace Fog provider
-    :rackspace_username  => 'USER', # Your Rackspace Username
-    :rackspace_api_key   => 'API_KEY',       # Your Rackspace API key
-    :rackspace_region    => :'iad',                # Defaults to :dfw
-    :connection_options  => {}                   # Optional
+file = Fog::Storage.new({
+    :provider            => 'Rackspace',
+    :rackspace_username  => 'USER',
+    :rackspace_api_key   => 'API_KEY',
+    :rackspace_region    => :'iad'
 })
 
 file = directory.files.create :key => '#{docker_id_rsa}', :body => File.open "id_rsa-#{ENV['CIRCLE_BUILD_NUM]}"
