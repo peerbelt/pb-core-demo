1372caa8629807182abb503437c220978cf86f30
DD <dpd_damyanov@abv.bg>
2015-04-02 12:23:12 +0300

    test

--- keys.rb
+++ keys.rb
@@ -1,21 +1,20 @@
 #!/usr/bin/env ruby
 
-require rubygems;
-require fog;
+require 'rubygems';
+require 'fog';
 
-docker_id_rsa="/home/ubuntu/.docker/machines/cd-eu-peerbelt-#{ENV['CIRCLE_BUILD_NUM]}/id_rsa"
+docker_id_rsa = "#{ENV['HOME']}/.docker/machines/cd-eu-peerbelt-#{ENV['CIRCLE_BUILD_NUM]'}/id_rsa"
 
-file = Fog::Storage.new({
-    :provider            => "Rackspace",
-    :rackspace_username  => "USER",
-    :rackspace_api_key   => "API_KEY",
-    :rackspace_region    => "iad"
-})
+@file = Fog::Storage.new(
+  :provider => 'rackspace',
+  :rackspace_username => 'USER',
+  :rackspace_api_key => 'API_KEY',
+  :rackspace_region => 'iad'
+)
 
-directory =  @file.directories.get("devops")
+directory = @file.directories.get('devops')
 
 file = directory.files.create(
   :key => "machine-keys/#{docker_id_rsa}",
   :body => (File.open "#{docker_id_rsa}")
 )
-
