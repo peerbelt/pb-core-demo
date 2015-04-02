2fb473376282645a836db76248d54b52acf5db91
DD <dpd_damyanov@abv.bg>
2015-04-02 12:07:54 +0300

    test

--- keys.rb
+++ keys.rb
@@ -6,16 +6,16 @@ require fog;
 docker_id_rsa="/home/ubuntu/.docker/machines/cd-eu-peerbelt-#{ENV['CIRCLE_BUILD_NUM]}/id_rsa"
 
 file = Fog::Storage.new({
-    :provider            => 'Rackspace',
-    :rackspace_username  => 'USER',
-    :rackspace_api_key   => 'API_KEY',
-    :rackspace_region    => :'iad'
+    :provider            => "Rackspace",
+    :rackspace_username  => "USER",
+    :rackspace_api_key   => "API_KEY",
+    :rackspace_region    => "iad"
 })
 
-directory =  @file.directories.get('devops')
+directory =  @file.directories.get("devops")
 
 file = directory.files.create(
-  :key => "machine-keys/#{docker_id_rsa},
-  :body => (File.open #{docker_id_rsa})
+  :key => "machine-keys/#{docker_id_rsa}",
+  :body => (File.open "#{docker_id_rsa}")
 )
 
