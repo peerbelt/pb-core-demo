7dfafb16fe20234cd19c3df636412a2bae580084
DD <dpd_damyanov@abv.bg>
2015-04-08 12:31:26 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-ID_RSA=/home/ubuntu/.docker/machines/cd-eu-peerbelt-$CIRCLE_BUILD_NUM/id_rsa
+ID_RSA=/home/ubuntu/.docker/machines/cd-iad-peerbelt-$CIRCLE_BUILD_NUM/id_rsa
 USER_DIR=/tmp
 
 
@@ -53,7 +53,7 @@ ssh -i $ID_RSA root@`./docker-machine ip` 'iptables -A OUTPUT -o eth0 -p tcp --s
 ssh -i $ID_RSA root@`./docker-machine ip` 'iptables -A INPUT -i eth0 -j DROP'
 
 # Start Papertrail
-ssh -i ../.docker/machines/cd-eu-peerbelt-$CIRCLE_BUILD_NUM/id_rsa root@`./docker-machine ip` 'sudo service remote_syslog start' 
+ssh -i ../.docker/machines/cd-iad-peerbelt-$CIRCLE_BUILD_NUM/id_rsa root@`./docker-machine ip` 'sudo service remote_syslog start' 
 # Clean up
 ssh -i $ID_RSA root@`./docker-machine ip` 'sudo sed -i s/PermitRootLogin\ yes/PermitRootLogin\ no/g /etc/ssh/sshd_config'
 ssh -i $ID_RSA root@`./docker-machine ip` 'sudo service ssh restart'
