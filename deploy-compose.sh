a7d3c18ff433f8b20179d19917e9dffbf1abcfe3
DD <dpd_damyanov@abv.bg>
2015-11-24 12:16:05 +0200

    test

--- deploy-compose.sh
+++ deploy-compose.sh
@@ -46,7 +46,7 @@ ssh -i $ID_RSA root@$SERVER_IP 'sudo docker load -i /home/peerbelt/pb-core-conso
 ssh -i $ID_RSA root@$SERVER_IP 'sudo docker load -i /home/peerbelt/pb-core-tracking-api-latest.tar'
 ssh -i $ID_RSA root@$SERVER_IP 'sudo docker load -i /home/peerbelt/pb-core-consumer-api-latest.tar'
 #ssh -i $ID_RSA root@$SERVER_IP 'sudo docker load -i /home/peerbelt/pb-core-digest-latest.tar'
-ssh -i $ID_RSA root@$SERVER_IP 'curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+ssh -i $ID_RSA root@$SERVER_IP 'curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 ssh -i $ID_RSA root@$SERVER_IP 'sudo chmod +x /usr/local/bin/docker-compose'
 scp -i $ID_RSA docker-compose.yml root@$SERVER_IP:/home/peerbelt
 ssh -i $ID_RSA root@$SERVER_IP 'sudo docker-compose --file /home/peerbelt/docker-compose.yml up -d' 
